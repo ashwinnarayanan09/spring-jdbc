c337765950c66a3ed75241ff731a7d2c6bf4c459
asna11 <ashwin.narayanan@comhem.com>
2019-05-22 23:19:22 +0200

    fixed rest controller

--- src/main/java/com/example/controller/AddressController.java
+++ src/main/java/com/example/controller/AddressController.java
@@ -5,15 +5,17 @@ import com.example.domain.AddressEntity;
 import com.example.service.AddressService;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/addressess")
+@Controller
+@RequestMapping("/api/addresses")
 
 public class AddressController {
 
@@ -34,6 +36,9 @@ public class AddressController {
 
         List<AddressEntity> addressList = addressService.readAllAddresses();
 
+        addressList.forEach(System.out::println);
         return ResponseEntity.ok(addressList);
     }
+
+
 }
---
+++ src/main/java/com/example/controller/GreetingController.java
@@ -0,0 +1,23 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+@RequestMapping(path = "/test")
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String HelloWorld() {
+
+        return "Hello";
+    }
+}
